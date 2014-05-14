fe64dda2c9e1751906ede2bbda77bb3c91441cb4
Peter <peterhsaxton@gmail.com>
2014-05-14 22:01:18 +0100

    started point object

--- spec/point_spec.js
+++ spec/point_spec.js
@@ -14,4 +14,18 @@ describe('Point', function(){
       expect(pt.y).toEqual(3);
     });
   });
+  describe('geometric operations', function(){
+    var Pt, p1, p2;
+    beforeEach(function(){
+      Pt = Utilities.Point;
+      p1 = Pt(2, 3);
+      p2 = Pt(4, 5);
+    });
+
+    it('adding 2 points', function(){
+      p3 = Pt.sum(p1, p2);
+      expect(p3.x).toEqual(6);
+      expect(p3.y).toEqual(8);
+    });
+  });
 });
