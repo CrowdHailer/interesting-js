95de2d4e91b8647e48ec2e1105b24a993ecf0223
Peter <peterhsaxton@gmail.com>
2014-05-14 13:03:44 +0100

    Added identity as defaults

--- spec/binary_add_spec.js
+++ spec/binary_add_spec.js
@@ -3,5 +3,13 @@ describe('Extending Binary functions', function(){
     it('should be able to add two numbers', function(){
       expect(add(2, 3)).toEqual(5);
     });
+
+    it('should default to adding zero when 1 argument', function(){
+      expect(add(2)).toEqual(2);
+    });
+
+    it('should return identity given no arguments', function(){
+      expect(add()).toEqual(0);
+    });
   });
 });
