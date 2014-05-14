e0eaae54ce5d4cfc3aaffbbc43f027d15e482de8
Peter <peterhsaxton@gmail.com>
2014-05-14 13:00:01 +0100

    Created basic add function

---
+++ spec/binary_add_spec.js
@@ -0,0 +1,7 @@
+describe('Extending Binary functions', function(){
+  describe('binary add', function(){
+    it('should be able to add two numbers', function(){
+      expect(add(2, 3)).toEqual(5);
+    });
+  });
+});
