9535cd1eaaca75d477d28cd83e30d749e26926a8
Peter <peterhsaxton@gmail.com>
2014-05-14 23:09:28 +0100

    Added object generalisation

--- spec/object_reduce_spec.js
+++ spec/object_reduce_spec.js
@@ -6,4 +6,28 @@ describe('object combining', function(){
     expect(p3.x).toEqual(6);
     expect(p3.y).toEqual(8);
   });
+  it('should generalise with 0 arg', function(){
+    var sum = inflate(addOb);
+    p3 = sum();
+    expect(p3.x).toEqual(0);
+    expect(p3.y).toEqual(0);
+  });
+  it('should generalise with 1 arg', function(){
+    var sum = inflate(addOb);
+    p3 = sum(p1);
+    expect(p3.x).toEqual(2);
+    expect(p3.y).toEqual(3);
+  });
+  it('should generalise with 2 arg', function(){
+    var sum = inflate(addOb);
+    p3 = sum(p1, p2);
+    expect(p3.x).toEqual(6);
+    expect(p3.y).toEqual(8);
+  });
+  it('should generalise with 5 arg', function(){
+    var sum = inflate(addOb);
+    p3 = sum(p1, p2, p1, p2, p2);
+    expect(p3.x).toEqual(16);
+    expect(p3.y).toEqual(21);
+  });
 });
